ad9d7bd2755ee4f094894adbd10569c5231e545b
Hedriss10 <hedrisgts@gmail.com>
2024-01-02 16:08:24 -0300

    "created app"

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ e com isso iremos colocar a regra de negocio com a as informações de execuçã
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from config import app_active, app_config
+
+
+# criando o app
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://root:1234@localhost/apirestflask'
